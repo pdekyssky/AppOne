074d0a780b5e585dcacc7ac7849339ead490af96
pdekyssky <p.dekyssky@gmail.com>
2023-01-10 10:51:48 +0100

    demo 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,33 @@
 import Book from "./components/Book"
 import './App.css'
 
+
+
 const App  = () => {
+
+    const books = [
+        {
+          id: 1,
+          image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-kamen-mudrcu-9788000061917.jpg.webp",
+          title: "Harry Potter a Kameň mudrcov"
+        },
+        {
+          id: 2,
+          image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/333420609/10.jpg.webp",
+          title: "Harry Potter a Tajomná komnata"
+        },
+        {
+          id: 3,
+          image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-vezen-z-azkabanu-9788000063393.jpg.webp",
+          title: "Harry Potter a Vezeň z Azkabanu"
+        },
+      ]
+
     return (
-        <div>
-            <h1> Booking</h1>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+        <div className="all-books">
+            <Book image={books[0].image} title={books[0].title}/>
+            <Book image={books[1].image} title={books[1].title}/>
+            <Book image={books[2].image} title={books[2].title}/>
         </div>
     )
 }
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,10 +1,15 @@
 import "./Book.css"
 import OrderButton from "./OrderButton"
 
-const Book = () => {
+const Book = (props) => {
     return ( 
-        <div>
-        <h2 className="book-name">Book name</h2>
+        <div className="one-book">
+        <h2 className="book-name">{props.title}</h2>
+        <img src={props.image} alt="book name" />
+        <p>lLorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fringilla pretium odio nec interdum. 
+        Cras et est risus. Morbi tristique ligula sit amet lectus auctor, sed egestas leo laoreet. Mauris luctus ligula ut pharetra sollicitudin.
+         Nulla a lectus sapien. Suspendisse mi justo, cursus at est sit amet, dictum convallis tellus.</p>
+         
         <OrderButton />
         </div>
     )
